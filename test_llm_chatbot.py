fd89388cb5dd494d2086867687f3ffe8999f87f3
zaidshk <zaid.ar.shaikh@gmail.com>
2024-12-23 10:47:35 +0400

    Complete assignment

--- test_llm_chatbot.py
+++ test_llm_chatbot.py
@@ -6,27 +6,27 @@ if __name__ == "__main__":
     if not os.path.exists("reports"):
         os.makedirs("reports")
 
-    # # Run predefined answers tests
-    # print("Running predefined answers tests...")
-    # pytest.main([
-    #     "tests/TC01_test_chatbot_predefined_answers.py",
-    #     "--html=reports/test_predefined_answers.html",
-    #     "--self-contained-html"
-    # ])
-    #
-    # # Run relevance answers tests
-    # print("Running relevance answers tests...")
-    # pytest.main([
-    #     "tests/TC02_test_chatbot_relevance_answers.py",
-    #     "--html=reports/test_relevance_answers.html",
-    #     "--self-contained-html"
-    # ])
-
-    # Run single thread/session performance test against the chatbot AI
-    print("Running performance tests")
+    # Run predefined answers tests
+    print("Running predefined answers tests...")
     pytest.main([
-        "tests/TC03_test_chatbot_performance.py",
-        "--html=reports/test_chatbot_performance.html",
+        "tests/TC01_test_chatbot_predefined_answers.py",
+        "--html=reports/test_predefined_answers.html",
         "--self-contained-html"
     ])
+
+    # Run relevance answers tests
+    print("Running relevance answers tests...")
+    pytest.main([
+        "tests/TC02_test_chatbot_relevance_answers.py",
+        "--html=reports/test_relevance_answers.html",
+        "--self-contained-html"
+    ])
+
+    # # Run single thread/session performance test against the chatbot AI
+    # print("Running performance tests")
+    # pytest.main([
+    #     "tests/TC03_test_chatbot_performance.py",
+    #     "--html=reports/test_chatbot_performance.html",
+    #     "--self-contained-html"
+    # ])
 

a8f277c3f5e1f9720a4b169b8de9ba85f72508b5
zaidshk <zaid.ar.shaikh@gmail.com>
2024-12-23 10:43:43 +0400

    Complete assignment

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-API_KEY = "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"
+API_KEY = "Your API Key"
 GOLDEN_ANSWERS = "golden_answers.txt"
 RAG_doc="e_commerce_RAG_doc.txt"
